1d8529a98f9ff115f44d79c0c4c9a570d12c4efc
Alexandr Degtyarev <Sasha20111997@gmail.com>
2022-11-10 21:05:41 +0500

    added Ex002 new ex

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 2!");
+﻿Console.Write ("Введите имя пользователя");
+string username = Console.ReadLine();
+Console.Write("Привет,");
+Console.Write(username);
